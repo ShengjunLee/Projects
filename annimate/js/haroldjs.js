601d00553f47d4f2fd78379ab84b6d927384e6a2
lishengjun <lishengjun@tju.edu.cn>
2017-03-09 10:48:28 +0800

    animate

---
+++ annimate/js/haroldjs.js
@@ -0,0 +1,13 @@
+	$().ready(function(){
+	
+	/*	$('#btn').on('click',function(){
+			$('.box').stop(false,true);
+		});
+		$('#btn2').on('click',function(){			
+					$('.box').animate({
+						left : '350px',
+						top : '350px'
+						},4000);								
+		});*/
+		
+	});
